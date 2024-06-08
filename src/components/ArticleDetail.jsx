913e27e595d2326cb4a0b7f6d8b6b54365372757
Nick Tolok <97984634+Diodant@users.noreply.github.com>
2024-06-08 22:33:50 +0300

    add

--- src/components/ArticleDetail.jsx
+++ src/components/ArticleDetail.jsx
@@ -34,6 +34,61 @@ const ArticleDetail = () => {
                 <Link to="/articles" className="back-link">Назад</Link>
                 <p className="date">{article.date}</p>
                 <h1 className="article-title">{article.title}</h1>
+                {article.id === 12 ? (
+          <>
+            <p>{article.content}</p>
+            <img src={article.imageUrl} alt={article.title} className="article-sub-image" />
+            <p>{article.content2}</p>
+            <p>{article.content3}</p>
+            <img src={article.imageUrl2} alt={article.title} className="article-sub-image" />
+            <p>{article.content4}</p>
+            <p>{article.content5}</p>
+            <p className='article-sub-title'>{article.content6}</p>
+            <p>{article.content7}</p>
+            <p>{article.content8}</p>
+            <p>{article.content9}</p>
+            <p className='article-sub-title'>{article.content10}</p>
+              <p>{article.content11}</p>
+              <p>{article.content12}</p>
+              <p>{article.content13}</p>
+              <p className='article-sub-title'>{article.content14}</p>
+              <p>{article.content15}</p>
+              <p>{article.content16}</p>
+              <p>{article.content17}</p>
+              <p className='article-sub-title'>{article.content18}</p>
+              <p>{article.content19}</p>
+              <p>{article.content20}</p>
+              <p>{article.content21}</p>
+              <p className='article-sub-title'>{article.content22}</p>
+              <p>{article.content23}</p>
+              <p>{article.content24}</p>
+              <p>{article.content25}</p>
+              <p className='article-sub-title'>{article.content26}</p>
+              <p>{article.content27}</p>
+              <p>{article.content28}</p>
+              <p>{article.content29}</p>
+              <p className='article-sub-title'>{article.content30}</p>
+              <p>{article.content31}</p>
+              <p>{article.content32}</p>
+              <p>{article.content33}</p>
+              <img src={article.imageUrl3} alt={article.title} className="article-sub-image" />
+              <p>{article.content34}</p>
+              <p>{article.content35}</p>
+              <p>{article.content36}</p>
+              <p className='article-sub-title'>{article.content37}</p>
+              <p>{article.content38}</p>
+              <p>{article.content39}</p>
+              <p>{article.content40}</p>
+              <p>{article.content41}</p>
+              <p>{article.content42}</p>
+              <p>{article.content43}</p>
+              <p>{article.content44}</p>
+              <p>{article.content45}</p>
+              <p>{article.content46}</p>
+              <p>{article.content47}</p>
+          </>
+        ) : (
+            <>
                 <p>{article.content}</p>
                 <p>{article.content2}</p>
                 <p>{article.content3}</p>
@@ -45,6 +100,8 @@ const ArticleDetail = () => {
                 <p>{article.content9}</p>
                 <p>{article.content10}</p>
                 <p>{article.content11}</p>
+                </>
+            )}
                 <div className="author">{article.author}</div>
             </div>
         </div>
